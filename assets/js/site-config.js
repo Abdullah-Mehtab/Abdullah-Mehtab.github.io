821b46e60a80ca8df688228df268853243193e9e
Abdullah Mehtab <abdullahmehtab666@gmail.com>
2026-05-13 09:49:42 +0500

    Configure Supabase comments

--- assets/js/site-config.js
+++ assets/js/site-config.js
@@ -1,5 +1,6 @@
 window.PORTFOLIO_CONFIG = {
-  supabaseUrl: "",
-  supabaseAnonKey: "",
-  commentsMode: "auto"
+  supabaseUrl: "https://zvuklviflletxyhniwdm.supabase.co",
+  supabaseAnonKey: "sb_publishable_almN_FPps-MxiLAF0Uypmw_jaCZ6VrI",
+  commentsMode: "auto",
+  commentsModeration: false
 };
